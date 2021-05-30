72512d92cc26764997ab51158e3f678ef35f1a69
Hano <hinsul3113@gmail.com>
2021-05-30 13:15:55 -0700

    handle #signin and #singup with github

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_200623) do
+ActiveRecord::Schema.define(version: 2021_05_30_195829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,10 @@ ActiveRecord::Schema.define(version: 2020_06_04_200623) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "is_admin", default: false
+    t.string "uid"
+    t.string "provider"
+    t.string "oauth_token"
+    t.string "oauth_raw_data"
     t.index ["email"], name: "index_users_on_email"
   end
 
